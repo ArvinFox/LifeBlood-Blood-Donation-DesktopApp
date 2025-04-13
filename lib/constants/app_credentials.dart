6981b6436b37b1f5eeb87944a8ccdadc5091717e
UminduH <73205708+UminduH@users.noreply.github.com>
2025-04-13 21:48:35 +0530

    Included app credentials file for initial setup

---
+++ lib/constants/app_credentials.dart
@@ -0,0 +1,9 @@
+class AppCredentials {
+  // Firebase Credentials
+  static const String apiKey = "";
+  static const String authDomain = "";
+  static const String projectId = "";
+  static const String storageBucket = "";
+  static const String messagingSenderId = "";
+  static const String appId = "";
+}
